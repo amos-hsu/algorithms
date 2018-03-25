be7aee0a1f54a09ff6ab0a8a5f578be9577cf901
amos-hsu <751654494@qq.com>
2018-03-25 22:29:30 +0800

    singleton

---
+++ sword2offer/problem1/Singleton.java
@@ -0,0 +1,61 @@
+/*
+单例设计模式：
+    只能有一个实例；
+    自己创建唯一实例
+    提供外部访问方法
+代码：
+    私有构造函数
+    建立本类对象
+    提供公有方法获取实例对象
+*/
+/*
+饿汉式：线程安全，耗费资源
+public class singele{
+    private static final Single instance = new Single();
+    private Single(){
+
+    }
+    public static Single getInstance(){
+        return instance;
+    }
+} 
+懒汉式：多个线程调用时等待，耗费资源
+public class singele{
+    private static Single instance;
+    private Single(){
+
+    }
+    public synchonized static Single getInstance(){
+        if(instance == null)
+            intance = new Single();
+        return instance;
+    }
+} 
+*/
+//线程安全：双重检查锁（同步代码块）,volatile关键字禁止指令重排序
+public class Singele1{
+    private volatile static Single1 instance;
+    private Single1(){
+
+    }
+    public synchronized static Single1 getInstance(){
+        if(instance == null)
+            synchronized(Singele1.class){
+                if(instance == null)
+                    intance = new Single();
+            }
+        return instance;
+    }
+} 
+//线程安全：静态内部类，
+public class Single2{
+    private Single2(){
+
+    }
+    private static class InstanceHolder{
+        public static Single2 instance = new Single2();
+    }
+    public static Single2 getInstance(){
+        return InstanceHolder.instance;
+    }
+}
