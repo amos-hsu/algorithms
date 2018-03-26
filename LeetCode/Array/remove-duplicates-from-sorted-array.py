73e71d8d0be11f50e41b1d221ec58eb6f255ef66
amos-hsu <751654494@qq.com>
2018-03-26 10:14:01 +0800

    mod so;lution

--- LeetCode/Array/remove-duplicates-from-sorted-array.py
+++ LeetCode/Array/remove-duplicates-from-sorted-array.py
@@ -12,7 +12,7 @@
 # Your function should return length = 2, and A is now [1,2].
 # 
 # 思路
-# 逆向思维，删除不重复元素，也就是找unique elements，并一个一个插入到数组最前面
+# 逆向思维，删除重复元素，也就是找unique elements，并一个一个插入到数组最前面
 # 插入的indexing label使用新建的index，与原数组的label比较
 # 注意：这里是sorted array
 
