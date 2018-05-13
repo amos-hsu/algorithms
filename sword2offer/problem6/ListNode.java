b7c6449bd7fdd252e58e360935106b031e0652f8
amos-hsu <751654494@qq.com>
2018-05-13 11:44:16 +0800

    list node

--- sword2offer/problem6/ListNode.java
+++ sword2offer/problem6/ListNode.java
@@ -1,12 +1,34 @@
 //链表
 
-class ListNode{
+class Node{
     int val;
-    ListNode next = null;
-    ListNode(int val){
+    Node next = null;
+    Node(int val){
         this.val = val;
     }
+}
+class MyLinkedList{
+    Node head = null;
+    /**向链表插入数据
+     * @param val:插入的数据内容
+     */
     public void addToTail(int val){
+        Node newNode = new Node(val);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node tmp = head;
+        while(tmp.next != null){
+            tmp = tmp.next;
+        }
+        tmp.next = newNode;
+    }
+    /**删除第index个结点
+     * @param index:待删除结点
+     * @return true:成功 
+     */
+    public Boolean deleteNode(int index){
         
     }
 }
