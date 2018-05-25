9fbdfda4d665ad60564865fbb32c345eed27897e
amos-hsu <751654494@qq.com>
2018-05-25 10:14:48 +0800

    stack implemented by two queue

--- sword2offer/problem9/StackWithTwoQueue.java
+++ sword2offer/problem9/StackWithTwoQueue.java
@@ -1,8 +1,5 @@
 /**
-入栈：将元素进队列A或B
-出栈：判断队列A中元素的个数是否为1，
-        如果等于1，则出队列，
-        否则将队列A中的元素依次出队列并放入队列B，直到队列A中的元素留下一个，然后队列A出队列，再把队列B中的元素出队列以此放入队列A中。
+ * 两个队列实现栈
  */
 
 import java.util.Queue;
@@ -14,15 +11,15 @@ public class Solution {
     Queue<Integer> queue2 = new Queue<Integer>();
     
     public void push(int node) {
-        if(queue1.isEmpty() && queue2.isEmpty()){
+        if(queue1.isEmpty() && queue2.isEmpty()){   //两个都为空，优先存入1
             queue1.add(node);
             return;
         }
-        if(queue1.isEmpty()){
+        if(queue1.isEmpty()){   //1为空，存入2
             queue2.add(node);
             return;
         }
-        if(queue2.isEmpty()){
+        if(queue2.isEmpty()){   //2为空，存入1
             queue1.add(node);
             return;
         }
@@ -44,3 +41,14 @@ public class Solution {
         return (Integer)null;
     }
 }
+
+/**
+ * 另一种方式：
+ * 入栈：将元素进队列A
+ * 出栈：判断队列A中元素的个数是否为1，
+ *       如果等于1，则出队列，
+ *       否则将队列A中的元素依次出队列并放入队列B，直到队列A中的元素留下一个，
+ *          然后队列A出队列，再把队列B中的元素出队列依次放入队列A中。
+ * 
+ * 比第一种复杂度高。
+ */
