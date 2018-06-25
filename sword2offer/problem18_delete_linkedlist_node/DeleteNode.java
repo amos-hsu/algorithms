0d5d32331bf3d8c4d637af0a31b655350a248223
amos-hsu <751654494@qq.com>
2018-06-25 19:45:14 +0800

    delete listnode

---
+++ sword2offer/problem18_delete_linkedlist_node/DeleteNode.java
@@ -0,0 +1,30 @@
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val = val;
+    }
+}
+public class Solution{
+    
+    //给定一个链表和一个节点对象，在O(1)时间内删除该节点
+    public void deleteNode(ListNode head, ListNode delteNode){
+        if(head == nul || head.next == null || deleteNode == null)
+            return null;
+        if(deleteNode.next != null){
+            ListNode nextNode = deleteNode.next;
+            deleteNode.val = nextNode.val;
+            deleteNode.next = nextNode.next;
+        }
+        else if(head == deleteNode){
+            head = null;
+        }
+        else{
+            ListNode curNode = head;
+            while(curNode.next != deleteNode){
+                curNode = curNode.next;
+            }
+            curNode.next = null;
+        }
+    }
+}
