0f3ac560035872a158d5e5b17df8e26f9ac0b458
huiyuexu <751654494@qq.com>
2020-06-14 11:17:13 +0800

    update bfs.c

--- leetcode/bfs.c
+++ leetcode/bfs.c
@@ -16,13 +16,7 @@ struct TreeNode {
 
 #define null 0xffffff
 
-int TreeSize(struct TreeNode *root)
-{
-    if (root == NULL) {
-        return 0;
-    }
-    return TreeSize(root->left) + TreeSize(root->right) + 1;
-}
+#define MAXSIZE 10000
 
 int MaxDepthOfTree(struct TreeNode *root)
 {
@@ -33,29 +27,33 @@ int MaxDepthOfTree(struct TreeNode *root)
     int right = MaxDepthOfTree(root->right);
     return ((left > right) ? left : right) + 1;
 }
-
-#define MAXSIZE 1000
 int **levelOrder(struct TreeNode *root, int *returnSize, int **returnColumnSizes)
 {
-    if (root == NULL) return NULL;
-    int **res = NULL;
-    int *colSizes = NULL;
+    int depth = MaxDepthOfTree(root); // 返回的二维数组行数
+    if (depth == 0) {
+        *returnSize = 0;
+        return NULL;
+    }
+    int *colSizes = (int *)malloc(sizeof(int) * depth); // 返回的二维数组每行列数
+    int **res = (int **)malloc(sizeof(int *) * depth);   // 返回的二维数组
+    struct TreeNode *queue[MAXSIZE];
     int rear = 0;
     int front = 0;
     int level = 0;
-    int depth = MaxDepthOfTree(root);
-    colSizes = (int *)malloc(sizeof(int) * depth); // 返回的二维数组每行列数
-    res = (int **)malloc(sizeof(int *) * depth);   // 返回的二维数组
-    struct TreeNode *queue[MAXSIZE];
+    int i;
     queue[rear++] = root;
     while (front != rear) {
         int col = rear - front;
-        colSizes[level++] = col;
+        colSizes[level] = col;
+        res[level] = (int *)malloc(sizeof(int) * col);
         for(i = 0; i < col; i++) {
-            
+            struct TreeNode *cur = queue[front++];
+            res[level][i] = cur->val;
+            if (cur->left != NULL) queue[rear++] = cur->left;
+            if (cur->right != NULL) queue[rear++] = cur->right;
         }
+        level++;
     }
-
     *returnSize = depth;
     *returnColumnSizes = colSizes;
     return res;
