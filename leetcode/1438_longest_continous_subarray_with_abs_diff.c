54c1faf2f924fda2a4dce846b787a7cfc1a67536
huiyuexu <751654494@qq.com>
2021-01-10 20:15:38 +0800

    slide window #3 #1438

--- leetcode/1438_longest_continous_subarray_with_abs_diff.c
+++ leetcode/1438_longest_continous_subarray_with_abs_diff.c
@@ -46,3 +46,48 @@ int main()
         sizeof(nums) / sizeof(int), limit));
     return 0;
 }
+
+/* 202101j解法 */
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+int longestSubarray20200110(int* nums, int numsSize, int limit)
+{
+    // 最长子串长度 -- 滑动窗口
+    // 子串属性：任意元素绝对差<=limit -> 最大和最小元素差<=limit
+    int l = 0;
+    int r = 0;
+    int ans = 0; // 字串长度
+    if (nums == NULL || numsSize == 0) {
+        return 0;
+    }
+    int min = nums[0];
+    int max = nums[0]; 
+    // 滑动窗口
+    while (r < numsSize) {
+        // 窗口右边界更新为r，窗口内信息刷新
+        min = MIN(min, nums[r]);
+        max = MAX(max, nums[r]);
+        r++; // 窗口右边界右移，左闭右开区间。窗口长度为r-l
+        if (max - min <= limit && ans < r - l) {  // 子串属性符合条件，刷新窗口长度
+            ans = r - l;
+        }
+        // printf("##l=%d r=%d ans=%d max=%2d min=%2d diff=%d\n", l, r , ans, max, min, max-min);
+        while (r < numsSize && max - min > limit) { // 字串属性不满足条件, 需要收缩, 条件: 最大和最小元素差>limit
+            // printf("++l=%d r=%d ans=%d max=%2d min=%2d diff=%d\n", l, r, ans, max, min, max-min);
+            l++;
+            r++;
+            max = INT_MIN;
+            min = INT_MAX;
+            for (int i = l; i < r; i++) {
+                max = nums[i] > max ? nums[i] : max;
+                min = nums[i] < min ? nums[i] : min;
+            }
+            if (max - min <= limit && ans < r - l) { // 左右节点同时刷新，可能到达边界，需要刷新窗口条件
+                ans = r - l;
+            }
+            // printf("==l=%d r=%d ans=%d max=%2d min=%2d diff=%d\n", l, r, ans, max, min, max-min);
+        }
+    }
+    return ans;
+}
--- leetcode/3_longest_str_without_repeat_ch.c
+++ leetcode/3_longest_str_without_repeat_ch.c
@@ -18,3 +18,30 @@ int lengthOfLongestSubstring(char * s)
     }
     return max;
 }
+
+/* solution 2 */
+int lengthOfLongestSubstring(char * s)
+{
+    int hash[128] = { 0 };
+    int l = 0;
+    int r = 0;
+    int ans = 0;
+    int len = 0;
+    if (s == NULL || strlen(s) == 0) {
+        return 0;
+    }
+    while (s[r] != '\0') {
+        len++;
+        hash[s[r]] = 1;
+        if (ans < len) {
+            ans = len;
+        }
+        r++;
+        while (s[r] != '\0' && hash[s[r]] == 1) {
+            hash[s[l]] = 0;
+            l++;
+            len--;
+        }
+    }
+    return ans;
+}
